8b6ff9bfc698d87e085d293a617d538f016a5c71
Om-Srivastava-6 <omsrivastava6126@gmail.com>
2026-08-20 17:59:47 +0530

    qol fix 8

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const themeToggle = document.querySelector(".theme-toggle");
 const themeIcon = document.querySelector(".theme-icon");
+const themeLogo = document.querySelector("#theme-logo");
 
 themeToggle.addEventListener("click", () => {
 	const isLightMode = document.body.classList.toggle("light-mode");
@@ -8,9 +9,11 @@ themeToggle.addEventListener("click", () => {
 		themeToggle.setAttribute("aria-label", "Switch to dark mode");
 		themeToggle.setAttribute("aria-pressed", "true");
 		themeIcon.textContent = "☾";
+		themeLogo.src = "assets/logo2.png";
 	} else {
 		themeToggle.setAttribute("aria-label", "Switch to light mode");
 		themeToggle.setAttribute("aria-pressed", "false");
 		themeIcon.textContent = "☼";
+		themeLogo.src = "assets/logo.png";
 	}
 });
